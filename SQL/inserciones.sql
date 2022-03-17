634771dac15d6ddd87fe895821cee25b1d6cc977
Aljandro c <ramtilla19@chupameelpenco.nomames>
2022-03-17 15:27:55 -0500

    correcion script insercion en la bd

--- SQL/inserciones.sql
+++ SQL/inserciones.sql
@@ -13,11 +13,11 @@ VALUES ('00001', '001','Colombia');
 
 -- Regiones
 INSERT into lugar (idlugar, idtipolugar, lug_idlugar, nomlugar)
-VALUES ('00002', '002','00001','Andina'),
-		('00003', '002','00001','Caribe')
-		('00004', '002','00001','Pacifica')
-		('00005', '002','00001','Llanos Orientales')
-		('00006', '002','00001','Amazonia')
+VALUES 	('00002', '002','00001','Andina'),
+		('00003', '002','00001','Caribe'),
+		('00004', '002','00001','Pacifica'),
+		('00005', '002','00001','Llanos Orientales'),
+		('00006', '002','00001','Amazonia'),
 		
 -- Ciudades
 		('00007', '003','00002','Bogota'),
@@ -29,31 +29,31 @@ VALUES ('00002', '002','00001','Andina'),
 --llenado aeropuetos internacionales
 --paises
 INSERT into lugar (idlugar, idtipolugar, nomlugar)
-VALUES 	('00002', '001','Estados Unidos'),
-		('00003', '001','venezuela'),
-		('00004', '001','Brazil');
+VALUES 	('00013', '001','Estados Unidos'),
+		('00014', '001','venezuela'),
+		('00015', '001','Brazil');
 --estados usa
 INSERT into lugar (idlugar, idtipolugar, lug_idlugar, nomlugar)
-VALUES ('00013', '007','00002','Florida'),
-		('00014', '007','00002','New York'),
-		('00015', '007','00002','Texas'),
-		('00016', '007','00003','Zulia'),
-		('00017', '007','00003','Amazonia'),
-		('00018', '007','00004','Sao Pablo'),
+VALUES 	('00016', '007','00013','Florida'),
+		('00017', '007','00013','New York'),
+		('00018', '007','00013','Texas'),
+		('00019', '007','00014','Zulia'),
+		('00020', '007','00014','Amazonia'),
+		('00021', '007','00015','Sao Pablo'),
 --Ciudades
-		('00019', '004','00013','Miami')
-		('00020', '004','00014','New York'),
-		('00021', '004','00015','Dallas'),
-		('00022', '004','00016','Maracaibo'),
-		('00023', '004','00017','Puerto Ayacucho'),
-		('00024', '007','00018','Sao Pablo'),
+		('00022', '004','00016','Miami'),
+		('00023', '004','00017','New York'),
+		('00024', '004','00018','Dallas'),
+		('00025', '004','00019','Maracaibo'),
+		('00026', '004','00020','Puerto Ayacucho'),
+		('00027', '007','00021','Sao Pablo'),
 --Aeropuerto
-		('00025', '004','00019','Aeropuerto Internacional de Miami - MIA')
-		('00026', '004','00020','Aeropuerto Internacional John F. Kennedy'),
-		('00027', '004','00021','Aeropuerto Internacional de Dallas-Fort Worth'),
-		('00028', '004','00022','Aeropuerto Internacional de La Chinita'),
-		('00029', '004','00023','Aeropuerto Cacique Aramare'),
-		('00030', '007','00024','Aeropuerto Internacional de São Paulo-Guarulhos');
+		('00028', '004','00022','Aeropuerto Internacional de Miami - MIA'),
+		('00029', '004','00023','Aeropuerto Internacional John F. Kennedy'),
+		('00030', '004','00024','Aeropuerto Internacional de Dallas-Fort Worth'),
+		('00031', '004','00025','Aeropuerto Internacional de La Chinita'),
+		('00032', '004','00026','Aeropuerto Cacique Aramare'),
+		('00033', '007','00027','Aeropuerto Internacional de São Paulo-Guarulhos');
 
 --Dias de la semana
 INSERT into Dia (idDia, AnomDIa)
