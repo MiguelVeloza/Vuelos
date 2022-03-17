7fd573c72d33983327861dd27e009b4f616845ca
Aljandro c <ramtilla19@chupameelpenco.nomames>
2022-03-16 19:22:31 -0500

    inserciones a la bd segun requerimientos

--- SQL/inserciones.sql
+++ SQL/inserciones.sql
@@ -25,3 +25,24 @@ VALUES ('00002', '002','00001','Andina'),
 		('00010', '003','00003','Barranquilla'),
 		('00011', '003','00003','Cartagena'),
 		('00012', '003','00002','Popayan');
+--llenado aeropuetos internacionales
+--paises
+INSERT into lugar (idlugar, idtipolugar, nomlugar)
+VALUES 	('00002', '001','Estados Unidos'),
+		('00003', '001','venezuela'),
+		('00004', '001','Brazil');
+--estados usa
+INSERT into lugar (idlugar, idtipolugar, lug_idlugar, nomlugar)
+VALUES ('00013', '007','00002','Florida'),
+		('00014', '007','00002','New York'),
+		('00015', '007','00002','Texas'),
+		('00016', '007','00003','Zulia'),
+		('00017', '007','00003','Amazonia'),
+		('00018', '007','00004','Sao Pablo'),
+--Ciudades
+		('00019', '004','00013','Miami')
+		('00020', '004','00014','New York'),
+		('00021', '004','00015','Dallas'),
+		('00022', '004','00016','Maracaibo'),
+		('00023', '004','00017','Puerto Ayacucho'),
+		('00023', '007','00018','Sao Pablo');
